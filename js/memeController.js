8dc68c68ad03e5a394080b8c16b8db5bea4a6677
idanRozen103 <68657577+idanRozen103@users.noreply.github.com>
2020-08-17 00:14:57 +0300

    Add files via upload

--- js/memeController.js
+++ js/memeController.js
@@ -3,6 +3,7 @@
 // var gKeywords = {'happy': 0,'funny puk': 0} 
 var gCanvas;
 var gCtx;
+var gCurrImg;
 
 function renderEditingPage(currImg) {
     gCanvas = document.querySelector('#myCanvas');
@@ -13,12 +14,32 @@ function renderEditingPage(currImg) {
     createMeme(currImg)
 }
 
-function renderCanvas(img) {
-    var scale = Math.max(gCanvas.width / img.width, gCanvas.height / img.height);
-    var x = (gCanvas.width / 2) - (img.width / 2) * scale;
-    var y = (gCanvas.height / 2) - (img.height / 2) * scale;
-    gCtx.drawImage(img, x, y, img.width * scale, img.height * scale);
+function resizeCanvas() {
+    const elCanvas = document.querySelector('.canvas-container')
+    // var scale = Math.max(gCurrImg.height / gCurrImg.width, gCurrImg.width / gCurrImg.height)
+    var scale = gCurrImg.width / gCurrImg.height
+
+
+    console.log(elCanvas.offsetWidth);
+
+    if (elCanvas.offsetWidth < 500) {
+        gCanvas.width = elCanvas.offsetWidth;
+        gCanvas.height = gCanvas.width / scale
+        // gCanvas.height = (scale === 1) ? elCanvas.offsetHeight : ((gCurrImg.height < gCanvas.height / 2) ? gCurrImg.height * scale : (gCurrImg.height > gCanvas.height) ? gCanvas.height / scale : gCurrImg.height / scale)
+    }
+    else {
+        console.log(gCurrImg.width/ gCurrImg.height);
+        gCanvas.height = elCanvas.offsetHeight;
+        gCanvas.width =gCanvas.height * scale 
+        
+        // ((gCurrImg.width < gCanvas.width ) ? gCurrImg.width / scale : (gCurrImg.width > gCanvas.width) ? gCanvas.width / scale : gCurrImg.width / scale)
+        // : (gCurrImg.width > gCanvas.width) ? gCanvas.width / scale : gCurrImg.width / scale)
+    }
 }
+
+// gCanvas.height = elCanvas.offsetHeight;
+// gCanvas.width = (scale === 1) ? elCanvas.offsetWidth : ((gCurrImg.width > gCurrImg.height) ? gCurrImg.width * (gCanvas.height/gCanvas.width) : elCanvas.offsetWidth)
+
 
 function onGoToGallery() {
     document.querySelector('.main-gallery').style.display = 'grid'
@@ -28,12 +49,11 @@ function onGoToGallery() {
 function onSetTxt(txtValue) {
     if (gMeme.lines.length === 0) onAddLine()
     setTxt(txtValue)
-    redrawCanvas()
+    renderCanvas()
 }
 
-function redrawCanvas() {
-    let elImg = document.querySelector(`[data-id='${gMeme.selectedImgId}']`)
-    renderCanvas(elImg)
+function renderCanvas() {
+    gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
     drawText()
 }
 
@@ -47,8 +67,8 @@ function drawText() {
         gCtx.strokeStyle = 'black'
         gCtx.lineWidth = '2';
         gCtx.textAlign = line.align;
-        gCtx.strokeText(line.txt, line.position.x, line.position.y);
-        gCtx.fillText(line.txt, line.position.x, line.position.y);
+        gCtx.strokeText(line.txt, gCanvas.width / 2, line.position.y);
+        gCtx.fillText(line.txt, gCanvas.width / 2, line.position.y);
     });
 }
 
@@ -56,34 +76,38 @@ function drawImgFromlocal(currImg) {
     var img = new Image()
     img.src = currImg.url;
     img.onload = () => {
-        renderCanvas(img)
+        gCurrImg = img;
+        gCanvas.height = 450
+        gCanvas.width = 450
+        resizeCanvas()
+        renderCanvas()
     }
 }
 
 function onChangeTxtSize(size) {
     setTxtSize(size)
-    redrawCanvas()
+    renderCanvas()
 }
 
 function onChangeTxtDirection(to) {
     setTxtDirection(to)
-    redrawCanvas()
+    renderCanvas()
 }
 
 function onLineUpward() {
     moveLineUp()
-    redrawCanvas()
+    renderCanvas()
 }
 
 function onLineDownward() {
     moveLineDown()
-    redrawCanvas()
+    renderCanvas()
 }
 
 
 function onChangeFillColor(color) {
     setFillColor(color)
-    redrawCanvas()
+    renderCanvas()
 }
 
 
@@ -97,13 +121,13 @@ function onSwitchLine() {
 
 function onChangeFont(font) {
     changeFont(font)
-    redrawCanvas()
+    renderCanvas()
 
 }
 
 function onRemoveLine() {
     removeLine()
-    redrawCanvas()
+    renderCanvas()
 }
 
 
@@ -123,13 +147,13 @@ function uploadImg(elForm, ev) {
         <a class="upload btn fab control" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;"> Share Meme
         </a>`
     }
-
     doUploadImg(elForm, onSuccess);
 
 }
 
 function doUploadImg(elForm, onSuccess) {
-    document.querySelector('form .btn').style.zIndex = '-1'
+    document.querySelector('form .btn').style.zIndex = '-4'
+    document.querySelector('form .btn').style.position = 'absolute'
 
     var formData = new FormData(elForm);
     fetch('http://ca-upload.com/here/upload.php', {
